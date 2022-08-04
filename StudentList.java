3db8db5078aa2ade887f713b559ea5ea8959cdc4
omor-niloy <omor.niloy.0@gmail.com>
2022-08-04 23:26:27 +0600

    updated

--- StudentList.java
+++ StudentList.java
@@ -3,14 +3,15 @@ import java.text.*;
 import java.util.*;
 public class StudentList {
 
-	public static BufferedReader bufferedReader;
+	public static String names[];
 	public static String studentNames;
 	public static Constant constant = new Constant();
 
 	public static void ReadNames(){
 		try {
-			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(constant.FileNmae)));
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(constant.FileNmae)));
 			studentNames = bufferedReader.readLine();
+			names = studentNames.split(constant.split);
 		} catch(Exception e){
 
 		}
@@ -37,7 +38,6 @@ public class StudentList {
 
 			System.out.println(constant.dataLoad);
 			ReadNames();
-			String names[] = studentNames.split(constant.split);
 			for(String name : names) {
 				System.out.println(name);
 			}
@@ -47,41 +47,28 @@ public class StudentList {
 
 			System.out.println(constant.dataLoad);
 			ReadNames();
-			String names[] = studentNames.split(constant.split);
 			Random random = new Random();
-			int randomIndex = random.nextInt(names.length);
-			System.out.println(names[randomIndex]);
+			System.out.println(names[random.nextInt(names.length)]);
 			System.out.println(constant.dataLoaded);
 		}
 		else if(args[0].contains(constant.addName)){
 
 			System.out.println(constant.dataLoad);
 			ReadNames();
-			String student = args[0].substring(1);
-			studentNames = studentNames + constant.split + student;
-			Date date = new Date();
+			studentNames = studentNames + constant.split + args[0].substring(1);
 			DateFormat dateFormat = new SimpleDateFormat(constant.dateFormat);
-			String fd= dateFormat.format(date);
-			String updateText = studentNames + constant.lastUpdate + fd;
-			Write(updateText);
-
+			Write(studentNames + constant.lastUpdate + dateFormat.format(new Date()));
 			System.out.println(constant.dataLoaded);
 		}
 		else if(args[0].contains(constant.query)) {
 
 			System.out.println(constant.dataLoad);
 			ReadNames();
-			String names[] = studentNames.split(constant.split);
-			boolean done = false;
-			String student = args[0].substring(1);
-			for(int idx = 0; idx<names.length && (!done); idx++) {
-				if(names[idx].equals(student)) {
+			for(int idx = 0; idx<names.length; idx++) {
+				if(names[idx].equals(args[0].substring(1))) {
 					System.out.println(constant.Found);
-					done=true;
+					break;
 				}
-			}
-			if (!done) {
-				System.out.println(constant.notFound);
 			}
 
 			System.out.println(constant.dataLoaded);
@@ -90,22 +77,7 @@ public class StudentList {
 
 			System.out.println(constant.dataLoad);
 			ReadNames();
-			char array[] = studentNames.toCharArray();
-			boolean in_word = false;
-			int count=0;
-			for(char character : array) {
-				if(character == ' ' && in_word) {
-					count++;
-					in_word = false;
-				}
-				else {
-					in_word = true;
-				}
-			}
-			if (in_word) {
-				count++;
-			}
-			System.out.println(count + constant.wordsFound);
+			System.out.println(names.length + constant.wordsFound);
 			System.out.println(constant.dataLoaded);
 		}
 		else {
